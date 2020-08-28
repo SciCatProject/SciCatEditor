d647291960c0f4766ce4e78e7ed8c5a4a1c9cc10
Spencer Bliven <spencer.bliven@gmail.com>
2020-08-28 14:14:17 +0200

    Build site with webpack

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import JSONEditor from 'jsoneditor';
+import './css/main.css';
+//import psiLogo from './images/psilogo.png';
+import html from './index.html';
+//import '!style-loader!css-loader?modules!jsoneditor/dist/jsoneditor.min.css';
+import 'jsoneditor/dist/jsoneditor.min.css';
+
+function main() {
+    console.log("Running main()");
+    // create the editor
+    const container = document.getElementById("jsoneditor")
+    const options = {
+        mode: 'tree',
+        modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+        onError: function (err) {
+            alert(err.toString())
+        }
+    }
+
+    const editor = new JSONEditor(container, options)
+    editor.expandAll();
+
+    // set json
+    //const examples = require('./examples.json');
+
+    const examples = [require("./examples/em_metadata.json")];
+    editor.set(examples[0]);
+    editor.expandAll();
+
+    // Load a JSON document
+    FileReaderJS.setupInput(document.getElementById('loadDocument'), {
+        readAsDefault: 'Text',
+        on: {
+            load: function (event, file) {
+                editor.setText(event.target.result)
+            }
+        }
+    })
+
+    // Save a JSON document
+    document.getElementById('saveDocument').onclick = function () {
+        // Save Dialog
+        let fname = window.prompt("Save as...","metadata.json")
+
+        // Check json extension in file name
+        if (fname.indexOf(".") === -1) {
+            fname = fname + ".json"
+        } else {
+            if (fname.split('.').pop().toLowerCase() === "json") {
+                // Nothing to do
+            } else {
+                fname = fname.split('.')[0] + ".json"
+            }
+        }
+        const blob = new Blob([editor.getText()], { type: 'application/json;charset=utf-8' })
+        saveAs(blob, fname)
+    }
+
+    return editor
+}
+
+window.editor = main();
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images',
+                },
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    'file-loader?name=[name].[ext]',
+                    'extract-loader',
+                    'html-loader'
+                ],
+            }
+        ],
+    },
+};
