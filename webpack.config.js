723cebb32abbc69ea3fd6de7cdcc8617b4f6567e
Spencer Bliven <spencer.bliven@gmail.com>
2020-09-02 13:13:55 +0200

    Add accordion panel for editor controls
    - PSI branding
    - fix CI script

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
                     MiniCssExtractPlugin.loader,
                     'css-loader',
                     'postcss-loader',
+                    'resolve-url-loader',
                     'sass-loader',
                 ],
             },
